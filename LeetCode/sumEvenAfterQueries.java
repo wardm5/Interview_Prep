fe5375967921ad7987a03ac31fc4ba6bbab715f5
Misha Ward <wardm5@uw.edu>
2019-07-07 16:24:08 -0700

    Create sumEvenAfterQueries.java

---
+++ LeetCode/sumEvenAfterQueries.java
@@ -0,0 +1,33 @@
+/*
+    https://leetcode.com/problems/sum-of-even-numbers-after-queries/
+ */
+
+class Solution {
+    public int[] sumEvenAfterQueries(int[] A, int[][] queries) {
+        int[] arr = new int[A.length];
+        int sum = 0;
+        for (int i = 0; i < A.length; i++) {
+            if (A[i] % 2 == 0) 
+                sum+= A[i];
+        }
+        for (int i = 0; i < queries.length; i++) {
+            int old = A[queries[i][1]];
+            A[queries[i][1]] = A[queries[i][1]] + queries[i][0];
+            int val = A[queries[i][1]];
+            if (old % 2 == 0 &&  val % 2 == 0) {   // if old is even and is still even
+                sum = sum + (val - old);  // add difference
+            } else if (old % 2 == 0 && val % 2 != 0) {  // if old is even and goes odd
+                sum -= old;  // subtract the old val
+            } else if (old %  2 != 0 && val % 2 == 0) {  // if old is odd and now even
+                sum += val;  //  add the val to the sum
+            }
+            arr[i] = sum;
+        }
+        return arr;
+    }
+}
+
+/*
+    Runtime: 4 ms, faster than 97.09% of Java online submissions for Sum of Even Numbers After Queries.
+    Memory Usage: 58.8 MB, less than 84.28% of Java online submissions for Sum of Even Numbers After Queries.
+ */
