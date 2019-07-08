31ce992ba11cba539f7ce28d6b2d1f2af6058bb5
Misha Ward <wardm5@uw.edu>
2019-07-07 17:06:29 -0700

    Add comments

--- LeetCode/toLowerCase.java
+++ LeetCode/toLowerCase.java
@@ -5,15 +5,15 @@
 
 class Solution {
     public String toLowerCase(String str) {
-        StringBuilder result = new StringBuilder();
-        for(int i = 0; i < str.length(); i++) {
-            char ch = str.charAt(i);
-            if(ch >= 'A' && ch <= 'Z') {
-                ch += 'a' - 'A';
+        StringBuilder result = new StringBuilder();  // create StringBuilder for quick appending
+        for(int i = 0; i < str.length(); i++) {  // for each char
+            char ch = str.charAt(i);             // get char
+            if(ch >= 'A' && ch <= 'Z') {         // if char is within than all capital letter values
+                ch += 'a' - 'A';                 // update char to lowercase
             }
-            result.append(ch);
+            result.append(ch);                   // append char
         }
-        return result.toString();
+        return result.toString();                // convert to string and return.
     }
 }
 
