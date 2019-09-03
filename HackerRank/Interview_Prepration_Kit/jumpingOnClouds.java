22b270795a31eaaf17ee9bf45d50b74dfd619fd5
Misha Ward <wardm5@uw.edu>
2019-09-02 17:22:57 -0700

    Update jumpingOnClouds.java

--- HackerRank/Interview_Prepration_Kit/jumpingOnClouds.java
+++ HackerRank/Interview_Prepration_Kit/jumpingOnClouds.java
@@ -1,15 +1,15 @@
-    public static int jumpingOnClouds(int[] c) {
-        int count = 0;
-        for (int i = 0; i < c.length; i++) {
-            // System.out.println(c[i] + "   " + i);
-            if (i + 2 < c.length && c[i+2] == 0) {
-                i++;
-                count++;
-                continue;
-            } else if (i + 1 < c.length && c[i+1] == 0) {
-                count++;
-                continue;
-            }
+public static int jumpingOnClouds(int[] c) {
+    int count = 0;
+    for (int i = 0; i < c.length; i++) {
+        // System.out.println(c[i] + "   " + i);
+        if (i + 2 < c.length && c[i+2] == 0) {
+            i++;
+            count++;
+            continue;
+        } else if (i + 1 < c.length && c[i+1] == 0) {
+            count++;
+            continue;
         }
-        return count;
     }
+    return count;
+}
