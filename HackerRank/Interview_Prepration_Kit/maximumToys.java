e725ea409498cbaebf39b4c395e5a5fb37205153
Misha Ward <wardm5@uw.edu>
2019-09-02 17:35:53 -0700

    Create maximumToys.java

---
+++ HackerRank/Interview_Prepration_Kit/maximumToys.java
@@ -0,0 +1,12 @@
+static int maximumToys(int[] prices, int k) {
+    Arrays.sort(prices);
+    int sum = 0;
+    int count = 0;
+    for (int i = 0; i < prices.length; i++) {
+        if (sum + prices[i] < k) {
+            sum = sum + prices[i];
+            count++;
+        } 
+    }
+    return count;
+}
