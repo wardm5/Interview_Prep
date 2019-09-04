db24fe298d34f63ffe7b098b2f49672887bd8ce9
Misha Ward <wardm5@uw.edu>
2019-09-03 20:56:30 -0700

    Create canWinNim.java

---
+++ LeetCode/canWinNim.java
@@ -0,0 +1,15 @@
+/*
+    292. Nim Game
+    https://leetcode.com/problems/nim-game/
+ */
+
+class Solution {
+    public boolean canWinNim(int n) {
+        return (n % 4 != 0);
+    }
+}
+
+/*
+    Runtime: 0 ms, faster than 100.00% of Java online submissions for Nim Game.
+    Memory Usage: 32.8 MB, less than 7.69% of Java online submissions for Nim Game.
+ */
