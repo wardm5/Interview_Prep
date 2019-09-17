f9c659e088d87bcc117f2d0790af34b8a74d0678
Misha Ward <wardm5@uw.edu>
2019-09-17 15:32:54 -0700

    Update numJewelsInStones.java

--- LeetCode/Java/numJewelsInStones.java
+++ LeetCode/Java/numJewelsInStones.java
@@ -5,21 +5,17 @@
 
 class Solution {
     public int numJewelsInStones(String J, String S) {
-        HashSet<Character> set = new HashSet<>();
         int count = 0;
-        for (int i = 0; i < J.length(); i++) {
-            set.add(J.charAt(i));
-        }
         for (int i = 0; i < S.length(); i++) {
-            if (set.contains(S.charAt(i))) {
+            String temp = Character.toString(S.charAt(i));
+            if (J.contains(temp))
                 count++;
-            }
         }
         return count;
     }
 }
 
 /*
-    Runtime: 1 ms, faster than 95.38% of Java online submissions for Jewels and Stones.
-    Memory Usage: 34.7 MB, less than 99.76% of Java online submissions for Jewels and Stones.
+    Runtime: 1 ms, faster than 93.41% of Java online submissions for Jewels and Stones.
+    Memory Usage: 35 MB, less than 97.47% of Java online submissions for Jewels and Stones.
  */
