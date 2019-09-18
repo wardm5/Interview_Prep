6ee715a537bbd859ccf9477bd61a6703f917821f
Misha Ward <wardm5@uw.edu>
2019-09-17 23:13:37 -0700

    Update searchBST.swift

--- LeetCode/Swift/searchBST.swift
+++ LeetCode/Swift/searchBST.swift
@@ -5,20 +5,19 @@
 
 class Solution {
     func searchBST(_ root: TreeNode?, _ val: Int) -> TreeNode? {
-        if (root == nil) {
+        if root == nil {
             return nil;
-        }
-        if (root!.val == val) {
+        } else if root!.val == val {
             return root;
-        } else if (root!.val > val) {
-            return searchBST(root!.left, val);
+        } else if root!.val > val {
+            return searchBST(root?.left, val);
         } else {
-            return searchBST(root!.right, val);
+            return searchBST(root?.right, val);
         }
     }
 }
 
 /*
-    Runtime: 176 ms, faster than 66.89% of Swift online submissions for Search in a Binary Search Tree.
-    Memory Usage: 21.3 MB, less than 100.00% of Swift online submissions for Search in a Binary Search Tree.
+    Runtime: 172 ms, faster than 93.92% of Swift online submissions for Search in a Binary Search Tree.
+    Memory Usage: 21.4 MB, less than 100.00% of Swift online submissions for Search in a Binary Search Tree.
  */
