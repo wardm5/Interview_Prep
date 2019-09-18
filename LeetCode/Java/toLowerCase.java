24d9fc55b74f623f430dbb34f62ca0290399f588
Misha Ward <wardm5@uw.edu>
2019-09-17 22:16:22 -0700

    Update toLowerCase.java

--- LeetCode/Java/toLowerCase.java
+++ LeetCode/Java/toLowerCase.java
@@ -5,19 +5,16 @@
 
 class Solution {
     public String toLowerCase(String str) {
-        StringBuilder result = new StringBuilder();  // create StringBuilder for quick appending
-        for(int i = 0; i < str.length(); i++) {  // for each char
-            char ch = str.charAt(i);             // get char
-            if(ch >= 'A' && ch <= 'Z') {         // if char is within than all capital letter values
-                ch += 'a' - 'A';                 // update char to lowercase
-            }
-            result.append(ch);                   // append char
+        StringBuilder build = new StringBuilder();
+        for (int i = 0; i < str.length(); i++) {
+            char temp = str.charAt(i);
+            build.append(Character.toLowerCase(temp));
         }
-        return result.toString();                // convert to string and return.
+        return build.toString();
     }
 }
 
 /*
     Runtime: 0 ms, faster than 100.00% of Java online submissions for To Lower Case.
-    Memory Usage: 34.3 MB, less than 99.80% of Java online submissions for To Lower Case.
+    Memory Usage: 33.9 MB, less than 98.70% of Java online submissions for To Lower Case.
  */
