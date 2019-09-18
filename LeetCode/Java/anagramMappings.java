3c9e9434c47dda3486fecdab79db6c1c04a1ac82
Misha Ward <wardm5@uw.edu>
2019-09-17 18:48:56 -0700

    Create anagramMappings.java

---
+++ LeetCode/Java/anagramMappings.java
@@ -0,0 +1,23 @@
+/*
+    760. Find Anagram Mappings
+    https://leetcode.com/problems/find-anagram-mappings/
+ */
+
+class Solution {
+    public int[] anagramMappings(int[] A, int[] B) {
+        HashMap<Integer, Integer> map = new HashMap<>();
+        for (int i = 0; i < A.length; i++) {
+            map.put(B[i], i);
+        }
+        int[] res = new int[A.length];
+        for (int i = 0; i < B.length; i++) {
+            res[i] = map.get(A[i]);
+        }
+        return res;
+    }
+}
+
+/*
+    Runtime: 1 ms, faster than 97.78% of Java online submissions for Find Anagram Mappings.
+    Memory Usage: 36 MB, less than 92.31% of Java online submissions for Find Anagram Mappings.
+ */
