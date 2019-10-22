a7f776e3a66bef7e7845eda23a6ee787246106a6
Misha Ward <wardm5@uw.edu>
2019-10-22 09:52:38 -0700

    Update uniquePaths.java

--- LeetCode/Java/uniquePaths.java
+++ LeetCode/Java/uniquePaths.java
@@ -23,3 +23,18 @@ class Solution {
     Runtime: 0 ms, faster than 100.00% of Java online submissions for Unique Paths.
     Memory Usage: 33.2 MB, less than 5.10% of Java online submissions for Unique Paths.
  */
+
+class Solution {
+    public int uniquePaths(int m, int n) {
+        int[][] arr = new int[m][n];
+        for (int i = 0; i < arr.length; i++) {
+            for (int j  = 0; j < arr[0].length; j++) {
+                if (i == 0 || j == 0)
+                    arr[i][j] = 1;
+                else 
+                    arr[i][j] = arr[i-1][j]+ arr[i][j-1];
+            }
+        }
+        return arr[m - 1][n- 1];
+    }
+}
