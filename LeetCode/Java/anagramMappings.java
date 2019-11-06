b688fc081d9538d6fbb8542eb472ab3c5c6c93cb
Misha Ward <wardm5@uw.edu>
2019-11-06 00:55:11 -0800

    added problems

--- LeetCode/Java/anagramMappings.java
+++ LeetCode/Java/anagramMappings.java
@@ -6,13 +6,11 @@
 class Solution {
     public int[] anagramMappings(int[] A, int[] B) {
         HashMap<Integer, Integer> map = new HashMap<>();
-        for (int i = 0; i < A.length; i++) {
+        for (int i = 0; i < A.length; i++)
             map.put(B[i], i);
-        }
         int[] res = new int[A.length];
-        for (int i = 0; i < B.length; i++) {
+        for (int i = 0; i < B.length; i++)
             res[i] = map.get(A[i]);
-        }
         return res;
     }
 }
--- LeetCode/Java/arrayPairSum.java
+++ LeetCode/Java/arrayPairSum.java
@@ -3,19 +3,15 @@
     https://leetcode.com/problems/array-partition-i/
  */
 
-class Solution {
-    public int arrayPairSum(int[] nums) {
-        int result = 0;
-        Arrays.sort(nums);
-        for (int i = 0; i < nums.length; i++) {
-            int val = Math.min(nums[i], nums[i + 1]);
-            // System.out.println(val);
-            result+= val;
-            i++;
-        }
-        return result;
-    }
-}
+ class Solution {
+     public int arrayPairSum(int[] nums) {
+         int result = 0;
+         Arrays.sort(nums);
+         for (int i = 0; i < nums.length - 1; i+=2)
+             result += Math.min(nums[i], nums[i + 1]);
+         return result;
+     }
+ }
 
 /*
     Runtime: 15 ms, faster than 41.19% of Java online submissions for Array Partition I.
