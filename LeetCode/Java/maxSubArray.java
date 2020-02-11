b8807b33ecb874bf7076bdaccf18cb042147fd30
Misha Ward <wardm5@uw.edu>
2020-02-11 14:47:14 -0800

    Update maxSubArray.java

--- LeetCode/Java/maxSubArray.java
+++ LeetCode/Java/maxSubArray.java
@@ -5,18 +5,30 @@
 
 class Solution {
     public int maxSubArray(int[] nums) {
-        int max = Integer.MIN_VALUE;
-        for (int i = 0; i < nums.length; i++) {
-            int tempSum = 0;
-            for (int j = i; j < nums.length; j++) {
-                tempSum += nums[j];
-                if (tempSum > max)
-                    max = tempSum;
-            }
+        int tempSum = nums[0];
+        int maxSum = nums[0];
+        for (int i = 1; i < nums.length; i++) {
+            tempSum = Math.max(nums[i], tempSum + nums[i]);
+            maxSum = Math.max(tempSum, maxSum);
         }
-        return max;
+        return maxSum;
     }
 }
+
+// class Solution {
+//     public int maxSubArray(int[] nums) {
+//         int max = Integer.MIN_VALUE;
+//         for (int i = 0; i < nums.length; i++) {
+//             int tempSum = 0;
+//             for (int j = i; j < nums.length; j++) {
+//                 tempSum += nums[j];
+//                 if (tempSum > max)
+//                     max = tempSum;
+//             }
+//         }
+//         return max;
+//     }
+// }
 
 /*
     Runtime: 45 ms, faster than 6.08% of Java online submissions for Maximum Subarray.
