d7d594c252fe531da68dee166455a89b918f4866
Misha Ward <wardm5@uw.edu>
2020-02-18 11:52:44 -0800

    adding solution problem statement for is symtric

--- LeetCode/Java/101_isSymmetric.java
+++ LeetCode/Java/101_isSymmetric.java
@@ -5,9 +5,10 @@
 /*-------------------------------------------------------------------------------
 
 Problem:
-Given the root node of a binary search tree (BST) and a value. You need to find
-the node in the BST that the node's value equals the given value. Return the
-subtree rooted with that node. If such node doesn't exist, you should return NULL.
+Given a binary tree, check whether it is a mirror of itself (ie, symmetric around
+its center).
+
+For example, this binary tree [1,2,2,3,4,4,3] is symmetric:
 
 -------------------------------------------------------------------------------*/
 
