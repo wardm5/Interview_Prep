d7ee040c45b0e23882155e08b0bd4f18e780c2c7
Misha Ward <wardm5@uw.edu>
2020-02-18 10:48:36 -0800

    adding number of Islands problem

---
+++ LeetCode/Java/Practice_Problems/numIslands.java
@@ -0,0 +1,36 @@
+class Solution {
+    public int numIslands(char[][] grid) {
+        int count = 0;
+        for (int  i = 0; i < grid.length; i++) {
+            for (int j = 0; j < grid[i].length; j++) {
+                if (grid[i][j] == '1') {
+                    flip(grid, new Point(i, j));
+                    count++;
+                }
+            }
+        }
+        return count;
+    }
+    public void flip(char[][] matrix, Point p) {
+        Queue<Point> q = new LinkedList<>();
+        q.add(p);
+        while (!q.isEmpty()) {
+            Point temp = q.remove();
+            if (temp.row < 0 || temp.col < 0 || temp.row >= matrix.length|| temp.col >= matrix[temp.row].length || matrix[temp.row][temp.col] == '0')
+                continue;
+            matrix[temp.row][temp.col] = '0';
+            q.add(new Point(temp.row+1, temp.col));
+            q.add(new Point(temp.row-1, temp.col));
+            q.add(new Point(temp.row, temp.col+1));
+            q.add(new Point(temp.row, temp.col-1));
+        }
+    }
+}
+class Point {
+    int row = 0;
+    int col = 0;
+    public Point(int row, int col) {
+        this.row = row;
+        this.col = col;
+    }
+}
