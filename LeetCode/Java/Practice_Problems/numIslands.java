f88abe4c4cd34e1b7f0a0b98484d5bf47122c0f3
Misha Ward <wardm5@uw.edu>
2020-02-18 10:57:44 -0800

    faster solution for number of Islands problem

--- LeetCode/Java/Practice_Problems/numIslands.java
+++ LeetCode/Java/Practice_Problems/numIslands.java
@@ -1,3 +1,30 @@
+// Faster solution
+class Solution {
+    public int numIslands(char[][] grid) {
+        int count = 0;
+        for (int  i = 0; i < grid.length; i++) {
+            for (int j = 0; j < grid[i].length; j++) {
+                if (grid[i][j] == '1') {
+                    flip(grid, i, j);
+                    count++;
+                }
+            }
+        }
+        return count;
+    }
+    public void flip(char[][] matrix, int i, int j) {
+        if (i < 0 || j < 0 || i >= matrix.length || j >= matrix[i].length || matrix[i][j] == '0')
+            return;
+        matrix[i][j] = '0';
+        flip(matrix, i+1, j);
+        flip(matrix, i-1, j);
+        flip(matrix, i, j+1);
+        flip(matrix, i, j-1);
+    }
+}
+// Runtime: 1 ms    99.98% of all Java submissions
+// Memory Usage: 42.1 MB   33.49% of all Java submissions
+
 class Solution {
     public int numIslands(char[][] grid) {
         int count = 0;
