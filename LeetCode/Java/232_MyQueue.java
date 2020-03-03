6313ef4e4204e1b2888fcfdf5c14e9c216e6a7f0
Misha Ward <wardm5@uw.edu>
2020-03-03 11:33:44 -0800

    updated file structure, adding queue with stacks problem

---
+++ LeetCode/Java/232_MyQueue.java
@@ -0,0 +1,91 @@
+//-------------------------------------------------------------------------------
+//    Misha Ward
+//-------------------------------------------------------------------------------
+
+/*-------------------------------------------------------------------------------
+
+Problem:
+Implement the following operations of a queue using stacks.
+
+push(x) -- Push element x to the back of queue.
+pop() -- Removes the element from in front of queue.
+peek() -- Get the front element.
+empty() -- Return whether the queue is empty.
+Example:
+
+MyQueue queue = new MyQueue();
+
+queue.push(1);
+queue.push(2);
+queue.peek();  // returns 1
+queue.pop();   // returns 1
+queue.empty(); // returns false
+
+-------------------------------------------------------------------------------*/
+
+//-------------------------------------------------------------------------------
+//    Approach
+//-------------------------------------------------------------------------------
+
+/*
+    1) Implement Queue with two stacks...
+*/
+
+//-------------------------------------------------------------------------------
+//    Soluton
+//-------------------------------------------------------------------------------
+
+class MyQueue {
+    Stack<Integer> s1;
+    Stack<Integer> s2;
+
+    /** Initialize your data structure here. */
+    public MyQueue() {
+        s1 = new Stack<>();
+        s2 = new Stack<>();
+    }
+
+    /** Push element x to the back of queue. */
+    public void push(int x) {
+        s1.push(x);
+    }
+
+    /** Removes the element from in front of queue and returns that element. */
+    public int pop() {
+        if (s2.empty()) {
+            while (!s1.empty()) {
+                s2.push(s1.pop());
+            }
+        }
+        return s2.pop();
+    }
+
+    /** Get the front element. */
+    public int peek() {
+        if (s2.empty()) {
+            while (!s1.empty()) {
+                s2.push(s1.pop());
+            }
+        }
+        return s2.peek();
+    }
+
+    /** Returns whether the queue is empty. */
+    public boolean empty() {
+        return (s1.isEmpty() && s2.isEmpty());
+    }
+}
+
+/**
+ * Your MyQueue object will be instantiated and called as such:
+ * MyQueue obj = new MyQueue();
+ * obj.push(x);
+ * int param_2 = obj.pop();
+ * int param_3 = obj.peek();
+ * boolean param_4 = obj.empty();
+ */
+
+/*
+    Runtime: 0 ms, faster than 100.00% of Java online submissions for Implement Queue using Stacks.
+    Memory Usage: 37.1 MB, less than 6.25% of Java online submissions for Implement Queue using Stacks.
+*/
--- LeetCode/Java/Array/485_findMaxConsecutiveOnes.java
+++ LeetCode/Java/Array/485_findMaxConsecutiveOnes.java
@@ -43,7 +43,6 @@ class Solution {
             }
             else
                 tempMax = 0;
-
         }
         return max;
     }
