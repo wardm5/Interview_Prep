30fda215769567fa238e499200cdf0952daf7823
Misha Ward <wardm5@uw.edu>
2020-03-22 15:26:00 -0700

    adding revise the select query problem

---
+++ HackerRank/SQL/revisingTheSelectQuery1.sql
@@ -0,0 +1,10 @@
+SELECT *
+FROM CITY
+WHERE POPULATION > 100000 AND CountryCode = 'USA' ;
+
+-- Output:
+-- 3878 Scottsdale USA Arizona 202705
+-- 3965 Corona USA California 124966
+-- 3973 Concord USA California 121780
+-- 3977 Cedar Rapids USA Iowa 120758
+-- 3982 Coral Springs USA Florida 117549
