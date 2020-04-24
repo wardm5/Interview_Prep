c29c1447a66d90825261b5e55c892e0e80ca586a
Michael Ward <wardm5@uw.edu>
2020-04-23 18:20:47 -0700

    added remove duplicates problem

--- Cracking_The_Coding_Interview/Solutions.java
+++ Cracking_The_Coding_Interview/Solutions.java
@@ -3,8 +3,14 @@ public class Solutions {
     public static void main(String[] args) {
         ListNode temp = new ListNode(5);
         temp.next = new ListNode(39);
-        LinkedList list = new LinkedList(temp);
-        list.add(6);
+        LinkedList list = new LinkedList();
+        list.createList();
         list.print();
+        list.removeDuplicates();
+        list.print();
+        System.out.println(list.returnKthToLast(2));
+        System.out.println(list.returnKthToLast(0));
+        System.out.println(list.returnKthToLast(6));
+        System.out.println(list.returnKthToLast(7));
     }
 }
--- Cracking_The_Coding_Interview/data_structures/LinkedList.java
+++ Cracking_The_Coding_Interview/data_structures/LinkedList.java
@@ -1,19 +1,88 @@
 package data_structures;
+import java.util.*;
 public class LinkedList {
-    ListNode head;
+    ListNode head;  // head of LinkedList
+    ListNode tail;
+    // constructors
     public LinkedList() {
         this.head = null;
+        this.tail = null;
     }
     public LinkedList(int val) {
-        head = new ListNode(val);
-    }
-    public ListNode getHead() {
-        return head;
+        this.head = new ListNode(val);
+        this.tail = head;
     }
     public LinkedList(ListNode newHead) {
         this.head = newHead;
+        ListNode temp = this.head;
+        while (temp.next != null)  {
+            temp = temp.next;
+        }
+        this.tail = temp;
     }
+
+    /*  CRACKING THE CODING INTERVIEW PROBLEMS - CHAPTER 2  */
+    // 2.1 Remove Duplicates
+    public void removeDuplicates() {
+        if (head == null || head == tail)
+            return;
+        Set<Integer> set = new HashSet<>();
+        ListNode temp = this.head;
+        ListNode prior = null;
+        while (temp.next != null && temp.next != this.tail) {
+            if (set.contains(temp.val)) {
+                prior.next = temp.next;
+                temp = temp.next;
+            } else {
+                set.add(temp.val);
+                prior = temp;
+                temp = temp.next;
+            }
+        }
+        if (set.contains(tail.val)) {
+            prior.next = null;
+            this.tail = prior;
+        }
+    }
+
+
+    /*  HELPER METHODS  */
+    // get head method - returns the head of the linked list
+    public ListNode getHead() {
+        return this.head;
+    }
+    public ListNode getTail() {
+        return this.tail;
+    }
+    // add function
+    public boolean add(int val) {
+        if (this.head == null) {
+            this.head = new ListNode(val);
+            this.tail = this.head;
+            return true;
+        }
+        ListNode temp = tail;
+        temp.next = new ListNode(val);
+        this.tail = temp.next;
+        return true;
+    }
+    // return new list - returns new list for problems
+    public void createList() {
+        LinkedList tempList = new LinkedList(7);
+        tempList.add(8);
+        tempList.add(2);
+        tempList.add(4);
+        tempList.add(4);
+        tempList.add(10);
+        tempList.add(3);
+        tempList.add(1);
+        tempList.add(2);
+        this.head = tempList.getHead();
+        this.tail = tempList.getTail();
+    }
+    // print method
     public void print() {
+        System.out.print("Printing List:   ");
         if (head == null) {
             System.out.println("Empty List");
             return;
@@ -25,7 +94,13 @@ public class LinkedList {
         }
         System.out.println();
     }
+    // replaces list with new head
     public void replaceList(ListNode newHead) {
         this.head = newHead;
+        ListNode temp = this.head;
+        while (temp.next != null)  {
+            temp = temp.next;
+        }
+        this.tail = temp;
     }
 }
