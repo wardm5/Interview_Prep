70758f49abf0865af0c0f32e0f5732d9412243a3
Misha Ward <wardm5@uw.edu>
2020-06-23 18:04:08 -0700

    adding binary search problem

---
+++ LeetCode/Java/Array/35_searchInsert.java
@@ -0,0 +1,59 @@
+//-------------------------------------------------------------------------------
+//    Misha Ward
+//-------------------------------------------------------------------------------
+
+/*-------------------------------------------------------------------------------
+
+Problem:
+35. Search Insert Position
+Easy
+
+Given a sorted array and a target value, return the index if the target is found.
+If not, return the index where it would be if it were inserted in order.
+
+You may assume no duplicates in the array.
+Example 1:
+Input: [1,3,5,6], 5
+Output: 2
+
+Example 2:
+Input: [1,3,5,6], 2
+Output: 1
+
+Example 3:
+Input: [1,3,5,6], 7
+Output: 4
+
+Example 4:
+Input: [1,3,5,6], 0
+Output: 0
+
+-------------------------------------------------------------------------------*/
+
+//-------------------------------------------------------------------------------
+//    Approach
+//-------------------------------------------------------------------------------
+
+/*
+    1) create ints for low  and high
+    2) while low is lower or equal to high
+        2.1) create mid int
+        2.2) if target is less than the current item, set high to mid +1
+        2.3) else set low to mid + 1
+    3) return low
+*/
+
+//-------------------------------------------------------------------------------
+//    Soluton
+//-------------------------------------------------------------------------------
+
+class Solution {
+    public String template() {
+
+    }
+}
+
+/*
+    Runtime: 0 ms, faster than 100.00% of Java online submissions for Remove Outermost Parentheses.
+    Memory Usage: 38.1 MB, less than 100.00% of Java online submissions for Remove Outermost Parentheses.
+*/
