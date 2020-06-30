15bb6e8534cc98e2cbc998701d6d433e375b8ec0
Misha Ward <wardm5@uw.edu>
2020-06-29 23:59:03 -0700

    adding updates to first unique char

--- LeetCode/Java/HashMap/387_firstUniqChar.java
+++ LeetCode/Java/HashMap/387_firstUniqChar.java
@@ -24,7 +24,7 @@ Note: You may assume the string contain only lowercase letters.
 //-------------------------------------------------------------------------------
 
 /*
-    1) create hashmap, loop through input string, store freqs to characters,
+    1) create hashmap (int array in updated code), loop through input string, store freqs to characters,
     2) loop through string again, if the count is 1, then return index
     3) return -1;
 */
@@ -35,32 +35,17 @@ Note: You may assume the string contain only lowercase letters.
 
 class Solution {
     public int firstUniqChar(String s) {
-        // LinkedHashMap<Character, List<Integer>> map = new LinkedHashMap<>();
-        // for (int i = 0; i < s.length(); i++) {
-        //     List<Integer> temp = map.getOrDefault(s.charAt(i), new ArrayList<>());
-        //     temp.add(i);
-        //     map.put(s.charAt(i), temp);
-        // }
-        // for (char c : map.keySet()) {
-        //     if (map.get(c).size() == 1)
-        //         return map.get(c).get(0);
-        // }
-        // return -1;
-        HashMap<Character, Integer> map = new HashMap<>();
-        // loop through list, keep track of freq
+        int[] map = new int[26];
         for (int i = 0 ; i < s.length(); i++)
-            map.put(s.charAt(i), map.getOrDefault(s.charAt(i), 0)+1);
-        // loop through list again, if you see item with freq 1, return that index
-        for (int i = 0; i < s.length(); i++) {
-            if (map.get(s.charAt(i)) == 1)
+            map[s.charAt(i) - 'a'] = map[s.charAt(i) - 'a'] + 1;
+        for (int i = 0; i < s.length(); i++)
+            if (map[s.charAt(i) - 'a'] == 1)
                 return i;
-        }
-        // return error
         return -1;
     }
 }
 
 /*
-    Runtime: 25 ms, faster than 41.52% of Java online submissions for First Unique Character in a String.
-    Memory Usage: 42.1 MB, less than 5.71% of Java online submissions for First Unique Character in a String.
+    Runtime: 10 ms, faster than 62.69% of Java online submissions for First Unique Character in a String.
+    Memory Usage: 39.8 MB, less than 74.60% of Java online submissions for First Unique Character in a String.
 */
